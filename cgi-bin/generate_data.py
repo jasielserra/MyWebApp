1402c4a6b1b37b9cfa019b0778b343e6a5760c3d
Jasiel Serra <jasiel_serra@hotmail.com>
2015-08-15 01:25:05 -0300

    Ajustado scripts CGI que fornece JSON a API do novo modelo.

--- cgi-bin/generate_data.py
+++ cgi-bin/generate_data.py
@@ -5,9 +5,10 @@ import json
 import athletemodel
 import yate
 
-athletes = athletemodel.get_from_store()
-form_data = cgi.FieldStorage()
 
-athlete_name = form_data['which_athlete'].value
+form_data = cgi.FieldStorage()
+athlete_id = form_data['which_athlete'].value
+athlete = athletemodel.get_athlete_from_id(athlete_id)
+
 print(yate.start_response("application/json"))
-print(json.dumps(athletes[athlete_name].as_dict))
+print(json.dumps(athlete['top3']))
--- cgi-bin/generate_names.py
+++ cgi-bin/generate_names.py
@@ -1,10 +1,10 @@
 #! /usr/bin/python3
 
 import json
-from athletemodel import get_names_from_store
+from athletemodel import get_namesID_from_store
 import yate
 
-athletes_names = get_names_from_store()
+names = get_namesID_from_store()
 
 print(yate.start_response('application/json'))
-print(json.dumps(sorted(athletes_names)))
+print(json.dumps(sorted(names)))
