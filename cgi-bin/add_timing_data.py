a75b6064bb811a6a2b5e828b648d1d5a8a102baa
Jasiel Serra <jasiel_serra@hotmail.com>
2015-08-16 10:53:39 -0300

    Ajustado scripts que faz inserções no Banco de Dados.

--- cgi-bin/add_timing_data.py
+++ cgi-bin/add_timing_data.py
@@ -1,20 +1,18 @@
 #! /usr/bin/python3
 
 import cgi
-import os
-import time
-import sys
+import sqlite3
 import yate
 
 print(yate.start_response('text/plain'))
-addr = os.environ['REMOTE_ADDR']
-host = os.environ['REMOTE_HOST']
-method = os.environ['REQUEST_METHOD']
-cur_time = time.asctime(time.localtime())
-print(host + ", " + addr + ", " + cur_time + ": " + method + ": ", end=' ',  file=sys.stderr)
 form = cgi.FieldStorage()
+the_id = form_data['Athlete'].value
+the_time = form_data['Time'].value
 
-for each_form_item in form.keys():
-    print(each_form_item  + '-> ' + form[each_form_item].value, end='  ', file=sys.stderr)
-print(file=sys.stderr)
+connection = sqlite3.connect('coachdata.sqlite')
+cursor = connection.cursor()
+cursor.execute("INSERT INTO timing_data (athlete_id,value) VALUES (?,?)", (the_id, the_time))
+
+connection.commit()
+connection.close()
 print('OK.')
